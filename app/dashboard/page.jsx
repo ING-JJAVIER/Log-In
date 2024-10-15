610bd4ffa4b8bf856e97c194587be41c99d93a09
Jose Javier <ing.josemartinezmendez@gmail.com>
2024-10-15 09:22:06 -0600

    first commit

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Dashboard() {
+    return (
+        <div className='text-center p-3 font-serif'>
+          
+            <h1 className='text-3xl font-semibold m-3 underline decoration-text '>Welcome</h1>
+            <p className='p-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat delectus eius maiores voluptas perferendis culpa quos, labore placeat ratione odio explicabo tenetur, est excepturi fugiat.</p>
+        </div>
+    )
+}
